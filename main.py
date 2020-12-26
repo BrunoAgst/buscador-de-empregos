477d8d74533ba98f3661040476282fe3fe3bafd8
Bruno Augusto <bruno_agst@hotmail.com>
2020-12-26 18:54:28 -0300

    criação dos templates e criação das rotas

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
+from flask import Flask, render_template
+from flask.globals import request
+from scrapping import scraping_vagas
 
+app = Flask('Buscador de Empregos')
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    job = request.args.get('job')
+    local = request.args.get('local')
+    
+    if local == None or local == "":
+        local = 'brasil'
+    else:
+        local = local.replace(" ", "-")
+        job = job.replace(" ", "-")
+
+    list_jobs = scraping_vagas(job, local)
+    
+    return render_template('result.html', jobs=list_jobs)
+
+app.run()
